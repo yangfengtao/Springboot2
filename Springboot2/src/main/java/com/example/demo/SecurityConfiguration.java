aa2f5941c76b7f488acfe1fa335d366885305f1d
qq360821994com <360821994@qq.com>
2019-01-29 15:30:01 +0800

    testThymeleaf

--- Springboot2/src/main/java/com/example/demo/SecurityConfiguration.java
+++ Springboot2/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -13,10 +13,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
-         super.configure(http);
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+	            .antMatchers( "/resources/**", "/signup" , "/about").permitAll()
+	            .antMatchers( "/admin/**").hasRole("ADMIN" )
+	            .antMatchers( "/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .and()
+            .httpBasic();
     }
-
+    
+    
     @Override
 	public void configure(WebSecurity web) throws Exception{
          super.configure(web);
